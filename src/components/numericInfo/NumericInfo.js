352f900c05e8a9fac998dbe208875dfcf281100c
Vinnichenko Anton <antonnugabest@gmail.com>
2022-10-09 18:55:29 +0300

    NumericInfo component was implemented

--- src/components/numericInfo/NumericInfo.js
+++ src/components/numericInfo/NumericInfo.js
@@ -1,5 +1,31 @@
+import Container from '../container/Container';
+import NumericItem from '../numericItem/NumericItem';
+
+import classes from './NumericInfo.module.scss';
+
+import established from '../../resources/numericInfo/established.svg';
+import workers from '../../resources/numericInfo/workers.svg';
+import customers from '../../resources/numericInfo/customers.svg';
+import surveillance from '../../resources/numericInfo/surveillance.svg';
+const numericItems = [
+  { img: established, title: 'Років', amount: '10' },
+  { img: workers, title: 'Працівників', amount: '100' },
+  { img: customers, title: 'Клієнтів', amount: '200' },
+  { img: surveillance, title: "Об'єктів", amount: '50' },
+];
+
 const NumericInfo = () => {
-  return <div></div>;
+  return (
+    <section className={classes.numericinfo}>
+      <Container>
+        <ul className={classes.numericinfo__list}>
+          {numericItems.map(({ img, title, amount }, index) => {
+            return <NumericItem key={index} img={img} title={title} amount={amount} />;
+          })}
+        </ul>
+      </Container>
+    </section>
+  );
 };
 
 export default NumericInfo;
--- src/components/numericItem/NumericItem.js
+++ src/components/numericItem/NumericItem.js
@@ -1,10 +1,55 @@
-const NumericItem = () => {
+import { useState, useEffect, useCallback } from 'react';
+
+import classes from './NumericItem.module.scss';
+
+const NumericItem = ({ img, title, amount }) => {
+  const [number, setNumber] = useState(0);
+  const [trigger, setTrigger] = useState(false);
+
+  const increment = useCallback(() => {
+    const counter = (length = 1000) => {
+      setNumber(0);
+      let n = amount;
+      let start = Date.now();
+      let end = start + length;
+      let interval = length / n;
+      const sInt = setInterval(() => {
+        let time = Date.now();
+        if (time < end) {
+          let count = Math.floor((time - start) / interval);
+          setNumber(count);
+        } else {
+          setNumber(n);
+          clearInterval(sInt);
+        }
+      }, interval);
+    };
+
+    counter();
+  }, [amount]);
+
+  document.addEventListener('scroll', async () => {
+    const element = document.getElementById('root');
+    const elementPosition = element.getBoundingClientRect().top;
+
+    if (window.pageYOffset > elementPosition) setTrigger(true);
+  });
+
+  useEffect(() => {
+    if (trigger) {
+      increment();
+    }
+  }, [trigger, increment]);
   return (
-    <div>
-      <img />
-      <div>Number</div>
-      <div>Title</div>
-    </div>
+    <li className={classes.numericitem}>
+      <div className={classes.numericitem__img}>
+        <img src={img} alt={title} />
+      </div>
+      <div className={classes.numericitem__amount} data-amount={amount}>
+        {number}
+      </div>
+      <p className={classes.numericitem__title}>{title}</p>
+    </li>
   );
 };
 
